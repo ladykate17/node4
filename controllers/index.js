99e3c4505fa3342b93da1d3de7db7e303dfa51aa
ladykate17 <stamm.katherine@gmail.com>
2015-07-24 11:47:31 -0600

    css and pic added

--- controllers/index.js
+++ controllers/index.js
@@ -9,10 +9,23 @@ var indexController = {
 		var locSlug = req.params.slug;
 
 		var foundLoc = location.findLoc(locSlug); 
+
+		if (locSlug === 'philippines') {
+			
+			res.render('last', {location : foundLoc});
+			
+		} else if ( foundLoc === undefined ) {
+
+			res.render('unknown', {location : foundLoc});
+
+		} else {
+
+			res.render('location', {location : foundLoc});
+
+		}
 		
-		// counter++
-		res.render('location', {location : foundLoc});
 	}
+
 };
 
 module.exports = indexController;
--- models/locations.js
+++ models/locations.js
@@ -1,19 +1,20 @@
 var allLocations = []
 
-var voyageStop = function(name, nextLoc, id){
+var voyageStop = function(name, img, nextLoc, id){
 	this.name     = name;
 	this.slug 	  = name.trim().toLowerCase().replace(/ /g, '-');
+	this.img 	  = img
 	this.nextLoc  = nextLoc;
 	this.nextSlug = nextLoc.trim().toLowerCase().replace(/ /g, '-');
 	allLocations.push(this); // this pushes the newStop to the allLocations array
 }
 
-new voyageStop('Seville', 'Canary Islands');
-new voyageStop('Canary Islands', 'Cape Verde');
-new voyageStop('Cape Verde', 'Strait of Magellan');
-new voyageStop('Strait of Magellan', 'Guam');
-new voyageStop('Guam', 'Philippines');
-new voyageStop('Philippines', '')
+new voyageStop('Seville', 'http://www.spainthenandnow.com/userimages/Seville%20in%2016th%20C%20Sanchez%20Coello%20Wikimedia.jpg', 'Canary Islands');
+new voyageStop('Canary Islands', 'http://www.acadian-cajun.com/canary1758.jpg', 'Cape Verde');
+new voyageStop('Cape Verde', 'http://www.columbia.edu/itc/mealac/pritchett/00maplinks/mughal/bertiusmaps/xcapeverde.jpg', 'Strait of Magellan');
+new voyageStop('Strait of Magellan', 'http://www.history.com/news/history-lists/files/2012/11/magellan-death.jpg', 'Guam');
+new voyageStop('Guam', 'http://www.mainlesson.com/books/haaren/modern/zpage107.gif', 'Philippines');
+new voyageStop('Philippines', 'http://www.landofthebrave.info/images/megellan-ships.jpg', '')
 
 var findLoc = function(slug){
 
